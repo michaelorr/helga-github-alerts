f92d73b4476fb56f81dc0cccd7c9694b35415cd1
Shaun Duncan <shaun.duncan@gmail.com>
2015-10-25 23:08:52 -0400

    Get the event name and payload and log it

--- helga-github-alerts/helga_github_alerts.py
+++ helga-github-alerts/helga_github_alerts.py
@@ -1,6 +1,17 @@
+import json
+import logging
+
 from helga.plugins.webhooks import route
+
+
+logger = logging.getLogger(__name__)
 
 
 @route(r'/github_alerts', methods=['POST'])
 def ghithub_alerts(request, client):
+    event_name = request.getHeader('X-Github-Event')
+    payload = json.load(request.content)
+
+    logging.info('Received github event %s. Payload: %s', event_name, payload)
+
     return 'Success!'
